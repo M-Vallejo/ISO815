3640ac640c33a777c73764154c230c9e87787fb2
Alvaro Martinez <armpmartinez@gmail.com>
2021-03-07 21:52:17 -0400

    Agregando Enums

---
+++ Practica Final/WebApiContabilidadSystem/Models/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiContabilidadSystem.Models
+{
+    public enum Estado
+    {
+        Inactivo = 0,
+        Activo =1,
+        Eliminado = 2
+    }
+
+    public enum TipoDocumento
+    {
+        Cedula = 0,
+        RNC = 1,
+        Pasaporte = 2
+    }
+    public enum Rol
+    {
+        Usuario = 0,
+        Administrador = 1
+    }
+    public enum TipoPersona
+    {
+        Fisica = 0,
+        Juridica = 1,
+    }
+}
