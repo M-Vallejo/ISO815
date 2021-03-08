0d95a039a84dee4ff32c32f26b40fffe34d272cf
Alvaro Martinez <armpmartinez@gmail.com>
2021-03-07 21:51:58 -0400

    internal asp.net core

--- Practica Final/WebApiContabilidadSystem/Models/Database/CONCEPTO_PAGO.cs
+++ Practica Final/WebApiContabilidadSystem/Models/Database/CONCEPTO_PAGO.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace WebApiContabilidadSystem.Models
 {
+    [Table("CONCEPTO_PAGO")]
+
     public class CONCEPTO_PAGO
     {
         [Key]
+        [JsonPropertyName("Concepto_pago_id")]
         public int CONCEPTO_PAGO_ID { get; set; }
         public string DESCRIPCION { get; set; }
         public int ESTADO { get; set; }
