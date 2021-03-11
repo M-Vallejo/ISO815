641f8218d6e3c7a69861ae864c8a2808855ce260
Alvaro Martinez <alvaro.martinez@pgr.gob.do>
2021-03-11 11:05:03 -0400

    .

--- Practica Final/WebApiContabilidadSystem/Models/Database/PROVEEDOR.cs
+++ Practica Final/WebApiContabilidadSystem/Models/Database/PROVEEDOR.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WebApiContabilidadSystem.Models
@@ -11,12 +12,19 @@ namespace WebApiContabilidadSystem.Models
     public class PROVEEDOR
     {
         [Key]
+        [JsonPropertyName("proveedor_id")]
         public int PROVEEDOR_ID { get; set; }
         public string NOMBRE { get; set; }
+        [JsonPropertyName("tipo_persona")]
         public int TIPO_PERSONA { get; set; }
+        [JsonPropertyName("tipo_documento")]
         public int TIPO_DOCUMENTO { get; set; }
+        [JsonPropertyName("numero_documento")]
         public string NUMERO_DOCUMENTO { get; set; }
+        [JsonPropertyName("balance")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal BALANCE { get; set; }
+        [JsonPropertyName("estado")]
         public int ESTADO { get; set; }
     }
 }
