cbceafb3dbef7c295a2718a1532f7dd160be5697
Michael Vallejo <dev.mvallejo0922@gmail.com>
2021-03-13 09:03:07 -0400

    Agregada validación de cédula

--- Practica Final/contabilidad-system-client/src/views/Proveedor.vue
+++ Practica Final/contabilidad-system-client/src/views/Proveedor.vue
@@ -297,7 +297,39 @@ export default {
                 return false;
             }
 
+            if(this.item.tipo_documento == 0 && !this.validateCedula(this.item.numero_documento)) {
+                numero_documento.focus();
+                return false;
+            }
             return true;
+        },
+        validateCedula(ced) {
+            var c = ced.replace(/-/g,'');  
+            var cedula = c.substr(0, c.length - 1);  
+            var verificador = c.substr(c.length - 1, 1);  
+            var suma = 0;  
+            var cedulaValida = 0;
+            if(ced.length < 11) { return false; }  
+            for (let i=0; i < cedula.length; i++) {  
+                let mod = "";  
+                if((i % 2) == 0){mod = 1} else {mod = 2}  
+                let res = cedula.substr(i,1) * mod;  
+                if (res > 9) {  
+                    res = res.toString();  
+                    let uno = res.substr(0,1);  
+                    let dos = res.substr(1,1);  
+                    res = eval(uno) + eval(dos);  
+                }  
+                suma += eval(res);  
+            }  
+            let el_numero = (10 - (suma % 10)) % 10;  
+            if (el_numero == verificador && cedula.substr(0,3) != "000") {  
+            cedulaValida = 1;
+            }  
+            else   {  
+            cedulaValida = 0;
+            }  
+            return cedulaValida;
         },
         saveItem() {
             if (this.validateData()) {
