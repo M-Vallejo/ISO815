945e4ce1fe42bf803d36703286135ffab03092f1
Michael Vallejo <dev.mvallejo0922@gmail.com>
2021-03-25 20:58:32 -0400

    Agregado proyecto de Práctica 8B

---
+++ PRACTICA8B/Receiver/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Receiver
+{
+    class Program
+    {
+        static string DocumentsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DOC");
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) 
+            {
+                string cedula = args[0];
+                string path = Path.Combine(DocumentsPath, cedula + ".png");
+                if (File.Exists(path))
+                    Process.Start("cmd", $"/c {path}");
+                else
+                    Console.WriteLine($"El archivo en la ruta \"{path}\" No existe");
+            }
+        }
+    }
+}
---
+++ PRACTICA8B/Sender/MainFrm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sender
+{
+    public partial class MainFrm : Form
+    {
+        string ProcessPath = @"..\..\..\..\Receiver\bin\Debug\net5.0\Receiver.exe";
+
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenDocument_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtCedula.Text))
+                OpenDocument(txtCedula.Text.Replace("-", ""));
+            else
+                MessageBox.Show("Debes digitar un número de cédula");
+        }
+
+        private void OpenDocument(string cedula)
+        {
+            try
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo(ProcessPath, cedula) 
+                { 
+                    CreateNoWindow = true
+                };
+                var process = Process.Start(processInfo);
+                process.WaitForExit();
+                process.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error",e.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            cbEstado.SelectedIndex = 0;
+        }
+    }
+}
