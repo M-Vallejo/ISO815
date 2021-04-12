6bf6fd78b3d5be4262d771f2c6f80cfb6eb53095
Alvaro Martinez <alvaro.martinez@pgr.gob.do>
2021-04-12 14:17:24 -0400

    Agregando settings y scalfolding contrador entradaDocumento

---
+++ Practica Final/WebApiContabilidadSystem/AccountingService/ServicioContabilidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiContabilidadSystem.AccountingService
+{
+    public class ServicioContabilidad
+    {
+
+        public string Procesar(int Total, int IdAuxiliar, string cuentadebito, string cuentaCredito, string descripcion)
+        {
+            return "";
+        }
+    }
+
+    
+}
