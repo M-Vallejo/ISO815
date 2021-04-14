7c44f94216a439014606e0012e7f37da0a45b90f
Alvaro Martinez <alvaro.martinez@pgr.gob.do>
2021-04-14 09:45:42 -0400

    agregando campos del servicioContabilidad y url en el settings

--- Practica Final/WebApiContabilidadSystem/AccountingService/ServicioContabilidad.cs
+++ Practica Final/WebApiContabilidadSystem/AccountingService/ServicioContabilidad.cs
@@ -7,7 +7,12 @@ namespace WebApiContabilidadSystem.AccountingService
 {
     public class ServicioContabilidad
     {
-
+        public string description { get; set; }
+        public int idAuxiliarSystem { get; set; }
+        public string movementType { get; set; }
+        public int account { get; set; }
+        public decimal seatAmount { get; set; }
+        public string entryDate { get; set; }
         public string Procesar(int Total, int IdAuxiliar, string cuentadebito, string cuentaCredito, string descripcion)
         {
             return "";
