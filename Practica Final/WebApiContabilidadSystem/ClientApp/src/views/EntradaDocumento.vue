f98abf5ded5b1db9e8e3a2849a0c73f85ae7e38a
Javier Mercedes <manuelmercedez10@gmail.com>
2021-04-15 00:13:16 -0400

    Agregado cmapos de documentos

--- Practica Final/WebApiContabilidadSystem/ClientApp/src/views/EntradaDocumento.vue
+++ Practica Final/WebApiContabilidadSystem/ClientApp/src/views/EntradaDocumento.vue
@@ -36,60 +36,113 @@
                             <v-card-text>
                                 <v-container>
                                     <v-form autocomplete="off" ref="form">
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field
-                                                v-model="item.numero_documento"
-                                                prepend-icon="person"
-                                                label="Numero de Documento"
-                                                required
-                                                type="number"
-                                                min='1'
-                                                id="numero_documento"
-                                            />
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field
-                                                v-model="item.numero_factura"
-                                                prepend-icon="person"
-                                                label="Numero de Factura"
-                                                required
-                                                type="number"
-                                                min='1'
-                                                id="numero_factura"
-                                            />
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field
-                                                v-model="item.numero_cheque"
-                                                prepend-icon="person"
-                                                label="Numero de Factura"
-                                                required
-                                                type="number"
-                                                min='1'
-                                                id="numero_factura"
-                                            />
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field
-                                                v-model="item.fecha_documento"
-                                                prepend-icon="person"
-                                                label="Fecha de Documento"
-                                                required
-                                                type="date"
-                                                id="fecha_documento"
-                                            />
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field
-                                                v-model="item.monto"
-                                                prepend-icon="person"
-                                                label="Monto"
-                                                required
-                                                type="number"
-                                                min="1"
-                                                id="monto"
-                                            />
-                                        </v-col>
+                                        <v-row>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-text-field
+                                                    v-model="item.numero_documento"
+                                                    prepend-icon="person"
+                                                    label="Numero de Documento"
+                                                    required
+                                                    type="number"
+                                                    min='1'
+                                                    id="numero_documento"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-text-field
+                                                    v-model="item.numero_factura"
+                                                    prepend-icon="person"
+                                                    label="Numero de Factura"
+                                                    required
+                                                    type="number"
+                                                    min='1'
+                                                    id="numero_factura"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-text-field
+                                                    v-model="item.numero_cheque"
+                                                    prepend-icon="person"
+                                                    label="Numero de cheque"
+                                                    required
+                                                    type="number"
+                                                    min='1'
+                                                    id="numero_cheque"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <template>
+                                                    <v-menu
+                                                        :close-on-content-click="false"
+                                                        transition="scale-transition"
+                                                        offset-y
+                                                        min-width="auto"
+                                                    >
+                                                        <template v-slot:activator="{ on, attrs }">
+                                                            <v-text-field
+                                                                v-model="item.fecha_documento"
+                                                                label="Fecha Documento"
+                                                                prepend-icon="mdi-calendar"
+                                                                readonly
+                                                                v-bind="attrs"
+                                                                v-on="on"
+                                                            ></v-text-field>
+                                                        </template>
+                                                        <v-date-picker
+                                                            ref="picker"
+                                                            v-model="item.fecha_documento"
+                                                            :max="currentDate"
+                                                            min="1950-01-01"
+                                                        >
+                                                        </v-date-picker>
+                                                    </v-menu>
+                                                </template>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-text-field
+                                                    v-model="item.monto"
+                                                    prepend-icon="person"
+                                                    label="Monto"
+                                                    required
+                                                    type="number"
+                                                    min="1"
+                                                    id="monto"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-select
+                                                    v-model="item.tipo_documento_id"
+                                                    prepend-icon="mdi-check-decagram"
+                                                    label="Tipo de Documento"
+                                                    required
+                                                    :items="tiposDocumentos"
+                                                    item-value="tipo_documento_id"
+                                                    item-text="descripcion"
+                                                    id="tipo_documento_id"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-select
+                                                    v-model="item.proveedor_id"
+                                                    prepend-icon="mdi-check-decagram"
+                                                    label="Proveedores"
+                                                    required
+                                                    :items="proveedores"
+                                                    item-value="proveedor_id"
+                                                    item-text="nombre"
+                                                    id="proveedor_id"
+                                                />
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="6">
+                                                <v-text-field
+                                                    v-model="item.condiciones"
+                                                    prepend-icon="person"
+                                                    label="Condiciones"
+                                                    type="text"
+                                                    id="condiciones"
+                                                />
+                                            </v-col>
+                                        </v-row>
                                     </v-form>
                                 </v-container>
                             </v-card-text>
@@ -129,24 +182,17 @@ export default {
 
             ],
             items: [],
-            estados: [
-                {
-                    "id": 1,
-                    "value": "Activo"
-                },
-                {
-                    "id": 0,
-                    "value": "Inactivo"
-                },
-            ],
             tiposDocumentos: [],
-            proveedores: []
+            proveedores: [],
+            currentDate: null
         }
     },
     mounted() {
         this.getTiposDocumentos();
         this.getProveedores();
         this.getEntradaDocumentos();
+        this.item.fecha_documento = this.setToday();
+        this.currentDate = this.setToday();
     },
     computed: {
         formTitle() {
@@ -184,7 +230,7 @@ export default {
                 numero_documento: null,
                 numero_factura: null,
                 numero_cheque: null,
-                fecha_documento: null,
+                fecha_documento: this.setToday(),
                 monto: null,
                 tipo_documento_id: null,
                 proveedor_id: null,
@@ -192,8 +238,25 @@ export default {
             });
             this.$refs.form.resetValidation();
         },
+        editItem(item) {
+            this.editedIndex = this.items.indexOf(item);
+            this.item = Object.assign({}, item);
+            this.modal = true;
+        },
+        validateData() {
+            
+        },
         saveItem() {
 
+        },
+        setToday() {
+            let today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1;
+            var yyyy = today.getFullYear();
+            if (dd < 10) dd = `0${dd}`;
+            if (mm < 10) mm = `0${mm}`;
+            return `${yyyy}-${mm}-${dd}`;
         }
     }
 }
